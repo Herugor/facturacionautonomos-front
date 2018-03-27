0e9a5c371b2c9af373e5f9c327abf45cb9ea798e
Alvaro J. Hurtado Villegas <alvaro.hurtado.villegas@gmail.com>
2018-03-27 17:12:21 +0200

    Feature 1/install vuetify (#4)
    * #1: install vuetify
    
    * #2:Limpiar datos test plantilla por defecto

---
+++ src/components/toolbar.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Facturacion Autonomos</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Hola userName</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'toolbar',
+  data () {
+    return {
+    }
+  }
+}
+
+</script>
+
