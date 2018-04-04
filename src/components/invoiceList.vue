85e66a2bb999a1742d9535fb1c486d26a5979663
herugor <herugor@gmail.com>
2018-04-04 15:56:39 +0200

    new component form in /add

--- src/components/invoiceList.vue
+++ src/components/invoiceList.vue
@@ -8,13 +8,13 @@
       <template slot="items" slot-scope="props">
         <td>{{ props.item.name }}</td>
         <td class="text-xs-right">{{ props.item.invoiceNum }}</td>
-        <td class="text-xs-right">{{ props.item.date }}</td>
-        <td class="text-xs-right">{{ props.item.description }}</td>
-        <td class="text-xs-right">{{ props.item.client }}</td>
-        <td class="text-xs-right">{{ props.item.B }}</td>
-        <td class="text-xs-right">{{ props.item.iva }}</td>
-        <td class="text-xs-right">{{ props.item.irpf }}</td>
-        <td class="text-xs-right">{{ props.item.total }}</td>
+        <td class="text-xs-right">{{ props.item.invoiceDate }}</td>
+        <td class="text-xs-right">{{ props.item.invoiceDesc }}</td>
+        <td class="text-xs-right">{{ props.item.invoiceCli }}</td>
+        <td class="text-xs-right">{{ props.item.invoiceTaxBase }}</td>
+        <td class="text-xs-right">{{ props.item.invoiceIva }}</td>
+        <td class="text-xs-right">{{ props.item.invoiceIrpf }}</td>
+        <td class="text-xs-right">{{ props.item.invoiceTotal }}</td>
       </template>
     </v-data-table>
   </template>
@@ -32,44 +32,44 @@ export default {
           value: 'name'
         },
         {text: 'Número de factura', value: 'invoiceNum'},
-        {text: 'date', value: 'date'},
-        {text: 'Descripción', value: 'description'},
-        {text: 'Cliente (texto corto)', value: 'client'},
-        {text: 'Base imponible', value: 'taxBase'},
-        {text: 'IVA', value: 'iva'},
-        {text: 'IRPF', value: 'irpf'},
-        {text: 'Total', value: 'total'}
+        {text: 'Fecha', value: 'invoiceDate'},
+        {text: 'Descripción', value: 'invoiceDesc'},
+        {text: 'Cliente (texto corto)', value: 'invoiceCli'},
+        {text: 'Base imponible', value: 'invoiceTaxBase'},
+        {text: 'IVA', value: 'invoiceIva'},
+        {text: 'IRPF', value: 'invoiceIrpf'},
+        {text: 'Total', value: 'invoiceTotal'}
       ],
       items: [
         {
           invoiceNum: 15,
-          date: '15/02/2018',
-          description: 'Tomatoes',
-          client: 'Tomatex',
-          taxBase: 100,
-          iva: 4,
-          irpf: 19,
-          total: 104
+          invoiceDate: '15/02/2018',
+          invoiceDesc: 'Tomatoes',
+          invoiceCli: 'Tomatex',
+          invoiceTaxBase: 100,
+          invoiceIva: 4,
+          invoiceIrpf: 19,
+          invoiceTotal: 104
         },
         {
           invoiceNum: 42,
-          date: '19/02/2018',
-          description: 'Apples',
-          client: 'Mamelux',
-          taxBase: 200,
-          iva: 4,
-          irpf: 12,
-          total: 208
+          invoiceDate: '19/02/2018',
+          invoiceDesc: 'Apples',
+          invoiceCli: 'Mamelux',
+          invoiceTaxBase: 200,
+          invoiceIva: 4,
+          invoiceIrpf: 12,
+          invoiceTotal: 208
         },
         {
           invoiceNum: 87,
-          date: '28/03/2018',
-          description: 'Crabs',
-          client: 'Rigoblas',
-          taxBase: 100,
-          iva: 10,
-          irpf: 15,
-          total: 110
+          invoiceDate: '28/03/2018',
+          invoiceDesc: 'Crabs',
+          invoiceCli: 'Rigoblas',
+          invoiceTaxBase: 100,
+          invoiceIva: 10,
+          invoiceIrpf: 15,
+          invoiceTotal: 110
         }
       ]
     }
